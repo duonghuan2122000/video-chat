0816c564aaaa92482b8d025f523db99302f5b429
Jocelyn <duong.huan2122000@gmail.com>
2021-10-15 07:32:22 +0700

    chỉnh sửa cấu hình

--- public/script.js
+++ public/script.js
@@ -3,11 +3,8 @@
  * @author CreatedBy: dbhuan (07/10/2021)
  */
 
-/**
- * Luồng video
- * @type {MediaStream}
- */
-let myVideoStream;
+// khởi tạo socket client
+const socket = io("/");
 
 // lấy ele khu vực hiển thị video
 const videoGrid = document.getElementById('video-grid');
@@ -18,14 +15,60 @@ const myVideo = document.createElement("video");
 // tắt tiếng video
 myVideo.muted = true;
 
+const user = prompt("Enter your name");
+
+// khởi tạo peer (WebRTC)
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: 443
+});
+
+/**
+ * Luồng video
+ * @type {MediaStream}
+ */
+let myVideoStream;
+
 // xử lý luồng video nhận được từ server
 navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true 
+    video: true
 })
-.then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
+    .then((stream) => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, stream);
+
+        // bắt sự kiện của peer
+        peer.on("call", call => {
+            call.answer(stream);
+
+            const video = document.createElement("video");
+
+            call.on("stream", userVideoStream => {
+                addVideoStream(video, userVideoStream);
+            })
+        });
+
+        // bắt sự kiện user kết nối
+        socket.on("user-connected", userId => {
+            connectToNewUser(userId, stream);
+        });
+    });
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+
+    const video = document.createElement("video");
+
+    call.on("stream", userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    });
+}
+
+// sự kiện peer mở kết nối
+peer.on("open", id => {
+    socket.emit("join-room", ROOM_ID, id, user);
 });
 
 /**
@@ -36,7 +79,6 @@ const addVideoStream = (video, stream) => {
     // thiết lập luồng cho video
     video.srcObject = stream;
 
-    // 
     video.addEventListener("loadedmetadata", () => {
         // hiển thị video
         video.play();
@@ -52,7 +94,7 @@ let controlVideo = document.getElementById("controlVideo");
 // sự kiện click ele controlVideo
 controlVideo.addEventListener('click', () => {
     const enabled = myVideoStream.getVideoTracks()[0].enabled;
-    if(enabled){
+    if (enabled) {
         // nếu cam đang mở thì sẽ thực hiện tắt cam
         myVideoStream.getVideoTracks()[0].enabled = false;
     } else {
--- server.js
+++ server.js
@@ -19,8 +19,22 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*"
+    }
+});
+const { ExpressPeerServer } = require('peer');
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
 // thiết lập view engine cho ứng dụng
 app.set('view engine', 'ejs');
+
+// sử dụng peerjs cho express
+app.use("/peerjs", peerServer);
 
 // thiết lập đường dẫn cho public static folder
 app.use(express.static("public"));
@@ -40,6 +54,19 @@ app.get("/", (req, res) => {
 app.get('/:room', (req, res) => {
     let roomId = req.params.room;
     res.render('room', { roomId });
+});
+
+// thiết lập kết nối connection
+io.on('connection', socket => {
+
+    // sự kiện tham gia phòng trong socket
+    socket.on("join-room", (roomId, userId) => {
+        // thực hiện join room
+        socket.join(roomId);
+
+        // broadcast tất cả các thành viên trong phòng có user mới
+        socket.to(roomId).emit("user-connected", userId);
+    })
 });
 
 // lắng nghe tại địa chỉ có port {PORT}
