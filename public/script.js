a629947ce7b5d3a284bc6fd8955ce70529b78538
lemanh-tuong <letuong05092000@gmail.com>
2021-10-30 16:00:57 +0700

    hoàn thiện tính năng disconnect user

---
+++ public/script.js
@@ -0,0 +1,112 @@
+const socket = io();
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+const user = prompt("Nhập tên của bạn");
+let userId;
+const peer = new Peer(undefined, {
+	path: "/peerjs",
+	host: "/",
+	port: 443
+});
+
+
+// sự kiện peer mở kết nối
+peer.on("open", id => {
+	userId = id;
+	socket.emit("join-room", ROOM_ID, userId, user);
+});
+
+/**
+ * Luồng video
+ * @type {MediaStream}
+ */
+let myVideoStream;
+
+// xử lý luồng video nhận được từ server
+navigator.mediaDevices.getUserMedia({
+	audio: true,
+	video: true
+})
+	.then((stream) => {
+		myVideoStream = stream;
+		addVideoStream({ video: myVideo, stream: myVideoStream, userId });
+
+		// bắt sự kiện của peer
+		peer.on("call", call => {
+			call.answer(stream);
+			const video = document.createElement("video");
+			call.on("stream", userVideoStream => {
+				addVideoStream({ video, stream: userVideoStream, userId: call.peer });
+			})
+		});
+
+		// bắt sự kiện user kết nối
+		socket.on("user-connected", userId => {
+			const call = peer.call(userId, stream);
+			const video = document.createElement("video");
+			call.on("stream", userVideoStream => {
+				addVideoStream({ video, stream: userVideoStream, userId });
+			});
+		});
+		// bắt sự kiện user ngắt kết nối
+		socket.on("user-disconnected", userId => {
+			const videoEls = Array.from(document.querySelectorAll(`[data-userId="${userId}"]`));
+			videoEls.forEach(video => video.remove());
+		});
+	}).catch(() => {
+		alert("Bật cam là F5 lại trang web");
+	});
+
+
+const addVideoStream = ({ video, stream, userId }) => {
+	video.srcObject = stream;
+	video.setAttribute('data-userId', userId)
+	video.addEventListener("loadedmetadata", () => {
+		video.play();
+		videoGrid.append(video);
+	});
+}
+
+
+// Xử lí các sự kiện của element
+const inviteButton = document.querySelector("#inviteButton");
+const muteButton = document.querySelector("#muteButton");
+const stopVideo = document.querySelector("#stopVideo");
+muteButton.addEventListener("click", () => {
+	const enabled = myVideoStream.getAudioTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false;
+		html = `<i class="fas fa-microphone-slash"></i>`;
+		muteButton.classList.toggle("background__red");
+		muteButton.innerHTML = html;
+	} else {
+		myVideoStream.getAudioTracks()[0].enabled = true;
+		html = `<i class="fas fa-microphone"></i>`;
+		muteButton.classList.toggle("background__red");
+		muteButton.innerHTML = html;
+	}
+});
+
+stopVideo.addEventListener("click", () => {
+	const enabled = myVideoStream.getVideoTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getVideoTracks()[0].enabled = false;
+		html = `<i class="fas fa-video-slash"></i>`;
+		stopVideo.classList.toggle("background__red");
+		stopVideo.innerHTML = html;
+	} else {
+		myVideoStream.getVideoTracks()[0].enabled = true;
+		html = `<i class="fas fa-video"></i>`;
+		stopVideo.classList.toggle("background__red");
+		stopVideo.innerHTML = html;
+	}
+});
+
+inviteButton.addEventListener("click", (e) => {
+	prompt(
+		"Sao chép link này và gửi cho người khác",
+		window.location.href
+	);
+});
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+	cors: {
+		origin: "*"
+	}
+});
+
+const peerServer = ExpressPeerServer(server, {
+	debug: true
+});
+
+// thiết lập view engine cho ứng dụng
+app.set('view engine', 'ejs');
+
+// sử dụng peerjs cho express
+app.use("/peerjs", peerServer);
+
+// thiết lập đường dẫn cho public static folder
+app.use(express.static("public"));
+
+/**
+ * GET: /
+ * Thực hiện redirect về trang /{00000000-0000-0000-0000-000000000000}
+ */
+app.get("/", (_, res) => {
+	res.redirect(`/${uuidv4()}`);
+});
+
+/**
+ * GET: /:room
+ * Render giao diện video chat
+ */
+app.get('/:room', (req, res) => {
+	let roomId = req.params.room;
+	res.render('room', { roomId });
+});
+
+// thiết lập kết nối connection
+io.on('connection', socket => {
+
+	// sự kiện tham gia phòng trong socket
+	socket.on("join-room", (roomId, userId) => {
+		socket.join(roomId);
+		socket.to(roomId).emit("user-connected", userId);
+		socket.on('disconnect', () => {
+			socket.to(roomId).emit("user-disconnected", userId);
+		})
+	})
+});
+
+// lắng nghe tại địa chỉ có port {PORT}
+server.listen(PORT);
